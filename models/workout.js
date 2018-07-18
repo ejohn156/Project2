2459eaf854245d1e4cbb4f7deeb54cfb40963996
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-07-18 12:13:55 -0400

    workout model

--- models/workout.js
+++ models/workout.js
@@ -1,13 +1,12 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Workouts = sequelize.define("Workouts", {
-      workout: {
-        type: DataTypes.OBJECT,
-        allowNull: false,
-        validate: {
-          len: [1]
+        workout: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
         }
-    }
     });
     return Workouts;
-  };
-  
+};
