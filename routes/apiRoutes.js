efd90e3a08680140ccdc7436c3de6abbea102ce1
Eric Johnson <ejohn156@uncc.edu>
2018-07-18 12:38:03 -0400

    set up api get and post workouts routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,28 @@
 var db = require("../models");
-
+var caber = require("caber")
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/workouts", function(req, res) {
+    db.Workouts.findAll({}).then(function(data) {
+      var workoutsApi = {}
+      data.forEach(function(workout){
+        workoutsApi += caber.workout(workout)
+      })
+      res.json(workoutsApi);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/workouts", function(req, res) {
+    db.Workouts.create(req.body).then(function(workout) {
+      res.json(workout);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
