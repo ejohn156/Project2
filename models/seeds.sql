12dfb8077c861830c9dcbdec3cae7705011574b4
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-07-19 01:09:44 -0400

    added to seeds

--- models/seeds.sql
+++ models/seeds.sql
@@ -2,24 +2,11 @@
 
 USE project_fitness;
 
-INSERT INTO workouts (workout) VALUES ("Monday Strenthen Lower Body\nSquat 135x5, 200x3, 225x4.\nLeg Press 135x5, 200x5, 250x5");
-INSERT INTO workouts (workout) VALUES ("Monday Strengthen Upper Body\nHammer Curls 30x10, 200x3, 225x4.\nBench Press 135x5, 200x5, 250x5");
+INSERT INTO workouts (workout) VALUES ("Monday Strengthen Lower Body\nBack Squat (3 sets), (10 reps).\nFront Squat (3 sets), (15 reps).\nLeg Press.\nStanding Calf Raises (3 sets), (15 reps).\nHip Thrust (4 sets), (8 reps).\nHip Adductor (3 sets), (15 reps).\nLateral Box Jump (5 sets), (7 reps).\nDeadlift (5 sets), (5 reps).\nHang Clean (5 sets), (5 reps).\nHang Power Clean (5 sets), (5 reps)");
+INSERT INTO workouts (workout) VALUES ("Monday Strengthen Upper Body\nHammer Curls (3 sets), (10 reps).\nBench Press (4 sets), (8 reps).\nDumbbell Curl (4 sets), (10 reps).\nReverse Barbell Curl (5 sets), (5 reps).\nSingle Arm Preacher Curl (5 sets), (5 reps).\nBarbell Incline Bench Press (4 sets), (8 reps).\nDumbbell Bench Press (5 sets), (6 reps).\nDumbbell Fly (5 sets), (8 reps).\nSkullcrusher (5 sets), (7 reps)");
 INSERT INTO workouts (workout) VALUES ("Monday Strengthen Core\nRussian Twist 5x22 (3 sets).\nCrunches 20x30 (3 sets).\nFlutter Kicks 20x3 (15 secs).\nPlate Twist 20x3");
 INSERT INTO workouts (workout) VALUES ("Monday Lean Cardio\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
 INSERT INTO workouts (workout) VALUES ("Monday Lean Core Circuit\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
 INSERT INTO workouts (workout) VALUES ("Monday Lean Watch Your Tone\nCrunches.\nBicycle Crunches.\nToe Touches.\nSide Crunches.\nFull Sit-Ups.\nKnee Bends.\nAlternate Heel Touches.\nScissor Kicks.\nBicep Curls.\nHammer Curls.\nOverhead Triceps Extensions.\nCross Chest Extensions");
 
-INSERT INTO workouts (workout) VALUES ("Monday Strenthen Lower Body\nSquat 135x5, 200x3, 225x4.\nLeg Press 135x5, 200x5, 250x5");
-INSERT INTO workouts (workout) VALUES ("Monday Strengthen Upper Body\nHammer Curls 30x10, 200x3, 225x4.\nBench Press 135x5, 200x5, 250x5");
-INSERT INTO workouts (workout) VALUES ("Monday Strengthen Core\nRussian Twist 5x22 (3 sets).\nCrunches 20x30 (3 sets).\nFlutter Kicks 20x3 (15 secs).\nPlate Twist 20x3");
-INSERT INTO workouts (workout) VALUES ("Monday Lean Cardio\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
-INSERT INTO workouts (workout) VALUES ("Monday Lean Core Circuit\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
-INSERT INTO workouts (workout) VALUES ("Monday Lean Watch Your Tone\nCrunches.\nBicycle Crunches.\nToe Touches.\nSide Crunches.\nFull Sit-Ups.\nKnee Bends.\nAlternate Heel Touches.\nScissor Kicks.\nBicep Curls.\nHammer Curls.\nOverhead Triceps Extensions.\nCross Chest Extensions");
-
-INSERT INTO workouts (workout) VALUES ("Monday Strenthen Lower Body\nSquat 135x5, 200x3, 225x4.\nLeg Press 135x5, 200x5, 250x5");
-INSERT INTO workouts (workout) VALUES ("Monday Strengthen Upper Body\nHammer Curls 30x10, 200x3, 225x4.\nBench Press 135x5, 200x5, 250x5");
-INSERT INTO workouts (workout) VALUES ("Monday Strengthen Core\nRussian Twist 5x22 (3 sets).\nCrunches 20x30 (3 sets).\nFlutter Kicks 20x3 (15 secs).\nPlate Twist 20x3");
-INSERT INTO workouts (workout) VALUES ("Monday Lean Cardio\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
-INSERT INTO workouts (workout) VALUES ("Monday Lean Core Circuit\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
-INSERT INTO workouts (workout) VALUES ("Monday Lean Watch Your Tone\nCrunches.\nBicycle Crunches.\nToe Touches.\nSide Crunches.\nFull Sit-Ups.\nKnee Bends.\nAlternate Heel Touches.\nScissor Kicks.\nBicep Curls.\nHammer Curls.\nOverhead Triceps Extensions.\nCross Chest Extensions");
 
