9d70cdce481b8d3c42c99da807fd87fc64a5c51f
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-07-19 00:13:18 -0400

    stashing

--- models/seeds.sql
+++ models/seeds.sql
@@ -3,8 +3,23 @@
 USE project_fitness;
 
 INSERT INTO workouts (workout) VALUES ("Monday Strenthen Lower Body\nSquat 135x5, 200x3, 225x4.\nLeg Press 135x5, 200x5, 250x5");
-INSERT INTO workouts (workout) VALUES ("Monday Strengthen Arms\nHammer Curls 30x10, 200x3, 225x4.\nBench Press 135x5, 200x5, 250x5");
+INSERT INTO workouts (workout) VALUES ("Monday Strengthen Upper Body\nHammer Curls 30x10, 200x3, 225x4.\nBench Press 135x5, 200x5, 250x5");
 INSERT INTO workouts (workout) VALUES ("Monday Strengthen Core\nRussian Twist 5x22 (3 sets).\nCrunches 20x30 (3 sets).\nFlutter Kicks 20x3 (15 secs).\nPlate Twist 20x3");
-INSERT INTO workouts (workout) VALUES ("Monday Core Circuit\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
-INSERT INTO workouts (workout) VALUES ("Monday Watch Your Tone\nCrunches.\nBicycle Crunches.\nToe Touches.\nSide Crunches.\nFull Sit-Ups.\nKnee Bends.\nAlternate Heel Touches.\nScissor Kicks.\nBicep Curls.\nHammer Curls.\nOverhead Triceps Extensions.\nCross Chest Extensions");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Cardio\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Core Circuit\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Watch Your Tone\nCrunches.\nBicycle Crunches.\nToe Touches.\nSide Crunches.\nFull Sit-Ups.\nKnee Bends.\nAlternate Heel Touches.\nScissor Kicks.\nBicep Curls.\nHammer Curls.\nOverhead Triceps Extensions.\nCross Chest Extensions");
+
+INSERT INTO workouts (workout) VALUES ("Monday Strenthen Lower Body\nSquat 135x5, 200x3, 225x4.\nLeg Press 135x5, 200x5, 250x5");
+INSERT INTO workouts (workout) VALUES ("Monday Strengthen Upper Body\nHammer Curls 30x10, 200x3, 225x4.\nBench Press 135x5, 200x5, 250x5");
+INSERT INTO workouts (workout) VALUES ("Monday Strengthen Core\nRussian Twist 5x22 (3 sets).\nCrunches 20x30 (3 sets).\nFlutter Kicks 20x3 (15 secs).\nPlate Twist 20x3");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Cardio\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Core Circuit\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Watch Your Tone\nCrunches.\nBicycle Crunches.\nToe Touches.\nSide Crunches.\nFull Sit-Ups.\nKnee Bends.\nAlternate Heel Touches.\nScissor Kicks.\nBicep Curls.\nHammer Curls.\nOverhead Triceps Extensions.\nCross Chest Extensions");
+
+INSERT INTO workouts (workout) VALUES ("Monday Strenthen Lower Body\nSquat 135x5, 200x3, 225x4.\nLeg Press 135x5, 200x5, 250x5");
+INSERT INTO workouts (workout) VALUES ("Monday Strengthen Upper Body\nHammer Curls 30x10, 200x3, 225x4.\nBench Press 135x5, 200x5, 250x5");
+INSERT INTO workouts (workout) VALUES ("Monday Strengthen Core\nRussian Twist 5x22 (3 sets).\nCrunches 20x30 (3 sets).\nFlutter Kicks 20x3 (15 secs).\nPlate Twist 20x3");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Cardio\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Core Circuit\n6 Inches (10 secs).\n8 Single Leg Crunch (each side).\n6 Inches (10 secs).\nSingle Leg Lifts (8).\n6 Inches (10 secs).\n6 Inch Crunches (10)");
+INSERT INTO workouts (workout) VALUES ("Monday Lean Watch Your Tone\nCrunches.\nBicycle Crunches.\nToe Touches.\nSide Crunches.\nFull Sit-Ups.\nKnee Bends.\nAlternate Heel Touches.\nScissor Kicks.\nBicep Curls.\nHammer Curls.\nOverhead Triceps Extensions.\nCross Chest Extensions");
 
