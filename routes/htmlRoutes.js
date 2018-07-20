dae34d0f2e631b24ac428c253ddc7267ecea4d3e
unknown <ewilsons@aol.comgit config --global user.email ewilsons@aol.comgit config --global user.email ewilsons@aol.com>
2018-07-19 22:51:10 -0400

    html work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,6 +8,11 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/profile", function(req, res) {
+    res.render("profile");
+  });
+
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
