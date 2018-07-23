08c8d077a7ef43dae29ad034299e9d3893611aec
Eric Johnson <ejohn156@uncc.edu>
2018-07-22 20:33:57 -0400

    added structure to indWorkout and put a button on the nav to get to workouts/all

--- public/js/bmi.js
+++ public/js/bmi.js
@@ -1,21 +1,21 @@
-var BMI = require("body-mass-index");
-$(".bmi-form").on("submit", function() {
-  var heightIn =
-    $(".heightIn")
-      .val()
-      .trim() + "in";
-  var heightFt =
-    $(".heightFt")
-      .val()
-      .trim() + "ft";
-  var height = heightFt + " " + heightIn;
-  var weight =
-    $(".weight")
-      .val()
-      .trim() + "lb";
-  var bmi = BMI(height, weight);
-  $(".bmiResult").text(bmi);
-});
+// var BMI = require("body-mass-index");
+// $(".bmi-form").on("submit", function() {
+//   var heightIn =
+//     $(".heightIn")
+//       .val()
+//       .trim() + "in";
+//   var heightFt =
+//     $(".heightFt")
+//       .val()
+//       .trim() + "ft";
+//   var height = heightFt + " " + heightIn;
+//   var weight =
+//     $(".weight")
+//       .val()
+//       .trim() + "lb";
+//   var bmi = BMI(height, weight);
+//   $(".bmiResult").text(bmi);
+// });
 
 $(".routes").on("click", function(event) {
   event.preventDefault();
